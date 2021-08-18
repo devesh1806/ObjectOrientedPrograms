2329616809d1b4e796158de688b44d557fd5ee1d
Devesh Bhangale1 <devesh.bhangale1@morningstar.com>
2021-08-18 12:17:47 +0530

    [Devesh]: Problem2, Added each Inventory value management for rice,wheat and pulses

--- src/com/objectorientedprograms/InventoryDataValue.java
+++ src/com/objectorientedprograms/InventoryDataValue.java
@@ -1,26 +1,37 @@
 package com.objectorientedprograms;
 
+import java.util.Scanner;
 
 public class InventoryDataValue {
-	public static void main(String[] args) {
-		String item1 = "Rice";
-        int price1=20;
-        int weight1=100;
-
-        String item2 = "Pulses";
-        int price2=30;
-        int weight2=150;
-
-        String item3 = "Wheats";
-        int price3=50;
-        int weight3=80;
-        Inventory[] invent=new Inventory[3];
-        invent[0]= new Inventory(item1,weight1,price1);
-		invent[1]= new Inventory(item2,weight2,price2);
-		invent[2]= new Inventory(item3,weight3,price3);
-		
+	
+	public Inventory[] invent;
+	
+	public InventoryDataValue() {
+		invent = new Inventory[3];
+		for(int i =0 ;i<invent.length;i++) {
+			addgrains(i);
+		}
+	}
+	
+	public void addgrains(int i) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter item name : ");
+		String item = sc.nextLine();
+		System.out.println("Enter weight");
+		int weight = sc.nextInt();
+		System.out.println("Enter price per kg:");
+		int price = sc.nextInt();
+		invent[i] = new Inventory(item,weight,price);
+	}
+	
+	public void displayvalue() {
 		for(int i=0;i<invent.length;i++) {
 			System.out.println("\n"+invent[i]);
 		}
+	}
+	
+	public static void main(String[] args) {
+		InventoryDataValue inventdata = new InventoryDataValue();
+		inventdata.displayvalue();
 	}
 }
